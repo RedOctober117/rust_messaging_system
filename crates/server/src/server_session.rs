46882912efa80198cd031f2300841465ff1f292b
RedOctober117 <dbcahilly3@gmail.com>
2025-04-05 21:16:17 -0500

    switch to channels started

--- crates/server/src/server_session.rs
+++ crates/server/src/server_session.rs
@@ -1,17 +1,12 @@
-use futures::io::FillBuf;
-use futures::TryFutureExt;
-use shared::message::{Message, MessageBody, Node, Response};
-use shared::user::User;
-use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter, Result};
-use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
-use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::RwLock;
+use shared::message::{Message, MessageBody, Node};
+use tokio::io::{AsyncBufReadExt, BufReader, BufWriter, Result};
+use tokio::net::TcpListener;
+use tokio::sync::mpsc;
+use tokio::sync::mpsc::Receiver;
 
 use std::collections::HashMap;
 use std::fmt::Display;
 use std::net::IpAddr;
-use std::ops::Deref;
-use std::sync::{Arc, Mutex, MutexGuard, PoisonError};
 
 pub struct ServerSession {
     address: (IpAddr, u16),
@@ -157,118 +152,44 @@ impl Display for AuthenticationError {
     }
 }
 
-//     let mut stream = tokio::io::BufReader::new(stream);
-//     let mut stream_buffer: Vec<u8> = vec![];
+// https://draft.ryhl.io/blog/actors-with-tokio/
+// https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=1ebab17a7a2f00a1fc7f37c58992d612
+pub struct TcpActorOrchestrator {
+    users: HashMap<u16, TcpActorHandle>,
+}
 
-//     stream.read_to_end(&mut stream_buffer).await.unwrap();
-//     let incoming_message = String::from_utf8(stream_buffer.into()).unwrap();
+pub struct TcpActor {
+    receiver: mpsc::Receiver<TcpActorMessage>,
+}
 
-//     let incoming_mess_len_raw = incoming_message.split(':').nth(0).unwrap();
-//     let incoming_message_len: u16 = incoming_mess_len_raw.parse().unwrap();
+pub enum TcpActorMessage {}
 
-//     let offset = incoming_mess_len_raw.len() + 1;
-//     // add 3 to offset for the xx: part of the stream_buffer
-//     let incoming_message_data = String::from_utf8(
-//         incoming_message[offset..(incoming_message_len + offset as u16).into()].into(),
-//     )
-//     .unwrap();
+impl TcpActor {
+    pub fn new(receiver: Receiver<TcpActorMessage>) -> Self {
+        Self { receiver }
+    }
 
-//     let deserialized_data = serde_json::from_str::<Message>(&incoming_message_data);
+    pub fn handle_message(&self, msg: TcpActorMessage) {
+        match msg {}
+    }
+}
 
-//     // println!(
-//     //     "raw data: {:?}\n data component: {}",
-//     //     incoming_message, incoming_message_data
-//     // );
+async fn run_actor(mut actor: TcpActor) {
+    while let Some(_) = actor.receiver.recv().await {
+        todo!()
+    }
+}
 
-//     match deserialized_data {
-//         Ok(message) => match message.get_destination() {
-//             Node::UserID(id) => {
-//                 let session = Arc::clone(&session_clone);
-//                 let mut writer = session.router.blocking_write();
-//                 writer.deref_mut().send_message(message).await.unwrap();
-//             }
-//             Node::Server => {
-//                 info!("Server received: {:?}", message);
-//                 match message.get_data() {
-//                     message::MessageBody::Text(s) => {
-//                         info!("server received message: {}", s)
-//                     }
-//                     message::MessageBody::File(vec) => todo!(),
-//                     message::MessageBody::User(user) => {
-//                         let session = Arc::clone(&session_clone);
-//                         let mut writer = session.router.blocking_write();
-//                         writer.deref_mut().route_user(user.id(), user.location());
-//                     }
-//                     message::MessageBody::Failure(_) => todo!(),
-//                     message::MessageBody::Request(request) => match request {
-//                         message::Request::ReqUserID => {
-//                             let id_length = session_clone.users.read().await.len();
-//                             // message::send_message(
-//                             //     &stream.into_inner(),
-//                             //     Message::new(
-//                             //         Node::Server,
-//                             //         message.get_source(),
-//                             //         message::MessageBody::Response(
-//                             //             message::Response::UserID((id_length + 1) as u16),
-//                             //         ),
-//                             //     ),
-//                             // )
-//                             // .await
-//                             // .unwrap();
-//                         }
-//                     },
-//                     message::MessageBody::Response(response) => todo!(),
-//                 }
-//             }
-//         },
-//         // ::Body(v) => match v.get_destination() {
-//         //     Node::UserID(id) => println!("destination: {}", id),
-//         //     Node::Server => println!("destination: server"),
-//         //     // Node::Server => match v.get_data() {
-//         //     //     MessageData::User(user) => {
-//         //     //         _ = session
-//         //     //             .users
-//         //     //             .write()
-//         //     //             .await
-//         //     //             .insert(user.id(), user.to_owned());
-//         //     //         session.router.write().await.route_user(user.id(), stream);
-//         //     //         println!("inserted user {}", user.id())
-//         //     //     }
-//         Err(e) => error!(
-//             "Could not process data '{:?}': {}",
-//             incoming_message_data, e
-//         ),
-//     };
+pub struct TcpActorHandle {
+    sender: mpsc::Sender<TcpActorMessage>,
+}
 
-// struct Router {
-//     addresses: HashMap<u16, (IpAddr, u16)>,
-// }
+impl TcpActorHandle {
+    pub fn new() -> Self {
+        let (sender, receiver) = mpsc::channel(8);
+        let actor = TcpActor::new(receiver);
+        tokio::spawn(run_actor(actor));
 
-// impl Router {
-//     pub fn new() -> Self {
-//         Self {
-//             addresses: HashMap::new(),
-//         }
-//     }
-//     pub fn route_user(&mut self, id: u16, loc: (IpAddr, u16)) {
-//         _ = self.addresses.insert(id, loc)
-//     }
-
-//     fn get_location(&mut self, id: &u16) -> Option<(IpAddr, u16)> {
-//         self.addresses.get(id).copied()
-//     }
-
-//     pub async fn send_message(&mut self, message: Message) -> shared::message::MessageResult<()> {
-//         if let Node::UserID(i) = &message.get_destination() {
-//             match self.get_location(i) {
-//                 Some(i) => {
-//                     let conn = TcpStream::connect(i).await?;
-//                     // message::send_message(&conn, message).await?;
-//                 }
-//                 None => warn!("wanted to send to user {}", i),
-//             }
-//         }
-
-//         Ok(())
-//     }
-// }
+        Self { sender }
+    }
+}
